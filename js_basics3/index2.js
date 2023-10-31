0488ba55109f394c401fb64fe58132eeeeea682e
ansariSaalim <ansarisaalim20@gmail.com>
2023-10-31 21:58:55 +0530

    Arrays & Strings

---
+++ js_basics3/index2.js
@@ -0,0 +1,14 @@
+//**************************Date and Time******************************
+let date = new Date('June 20 1998 07:18');
+console.log(date);
+
+let date1 = new Date(2004, 3, 3, 8);
+console.log(date1);
+console.log(date1.getDate());
+
+
+let date3 = new Date();
+date3.setFullYear(2001);
+console.log(date3);
+
+
