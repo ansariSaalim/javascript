c1e6d3ab3836b74dffd47f30935c9c7165fef5bb
ansariSaalim <ansarisaalim20@gmail.com>
2023-11-02 19:11:20 +0530

    Arrays & String

--- js_basics3/index3.js
+++ js_basics3/index3.js
@@ -1,4 +1,23 @@
 //****************************Arrays*****************************
 
-let arr = [1,2,3,4];
+let arr = [1,2,3,4,5];
 console.log(arr);
+
+//length
+console.log(arr.length);
+
+//Search
+console.log(arr[0]);
+
+//Insert
+//Insert at the end
+arr.push(10)
+console.log(arr);
+//Insert at the begin
+arr.unshift(22);
+console.log(arr);
+//Insert in the middle
+arr.splice(2, 0, 'a', 'b', 'c');
+console.log(arr);
+
+
